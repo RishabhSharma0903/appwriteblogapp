60ced00f486635b8d0840c2a51f3df92a69221d7
RishabhSharma0903 <risharma0903@gmail.com>
2024-07-30 17:24:09 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = '10px'}) {
+  return (
+    <>
+      <img width={width} src="../../src/assets/blogging.png" alt="logo" />
+    </>
+  )
+}
+
+export default Logo
