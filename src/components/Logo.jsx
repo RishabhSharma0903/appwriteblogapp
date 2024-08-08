38e5309b2b1f2eeb45bb424fa874d0a4b07035b4
Rishabh Sharma <104974230+RishabhSharma0903@users.noreply.github.com>
2024-08-08 21:57:01 +0530

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width = '10px'}) {
   return (
     <>
-      <img width={width} src="../src/assets/blogging.svg" alt="logo" />
+      <img width={width} src="./../../src/assets/blogging.svg" alt="logo" />
     </>
   )
 }
